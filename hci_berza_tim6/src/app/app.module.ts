2c6b643f699d2fc7e75996392d470e718205ecb8
Kristina Andrijin <andrijinkristina@gmail.com>
2023-04-07 21:15:57 +0200

    Added Angular Materials

--- hci_berza_tim6/src/app/app.module.ts
+++ hci_berza_tim6/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MaterialModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hci_berza_tim6/src/app/material.module.ts
+++ hci_berza_tim6/src/app/material.module.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@NgModule({
+  exports: [
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule
+  ]
 })
-export class AppComponent {
-  title = 'hci_berza_tim6';
-}
+export class MaterialModule { }
